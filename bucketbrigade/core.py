da652d3246f5e0375260fb5cc9d37767fa0d56de
Doug Hudgeon <doug.hudgeon@managedfunctions.com>
2024-01-16 12:24:33 +1100

    More raw code

--- bucketbrigade/core.py
+++ bucketbrigade/core.py
@@ -1,4 +1,5 @@
 import arrow
+import pandas as pd
 import re
 
 from decimal import ROUND_HALF_UP, ROUND_UP, Decimal, InvalidOperation
@@ -8,6 +9,8 @@ from xml.etree import ElementTree as ET
 from pydantic import (
     BaseModel,
     computed_field,
+    ValidationError,
+    parse_obj_as
 )
 
 def rx(n, x=2):
@@ -115,3 +118,45 @@ def parse_xml(xml_string):
     xml_string = remove_xml_namespace_prefixes(xml_string)
     tree = ET.ElementTree(ET.fromstring(xml_string))
     return etree_to_dict(tree.getroot())
+
+
+def validate_df(df: pd.DataFrame, model: Type[BaseModel], drop_missing=True, return_errors=False, print_errors=True) -> pd.DataFrame:
+    # Extract aliases and rename DataFrame columns based on aliases
+    field_to_alias = {field_name: field.alias for field_name, field in model.__annotations__.items() if hasattr(field, 'alias')}
+    df = df.rename(columns={v: k for k, v in field_to_alias.items()})
+
+    # Validate and collect errors
+    validated_rows, error_messages = [], []
+    for idx, row in df.iterrows():
+        try:
+            instance = parse_obj_as(model, row.to_dict())
+            validated_rows.append(instance.dict())
+        except ValidationError as e:
+            e = re.sub(r'For further information visit https://errors.pydantic.dev/\d+\.\d+/v/[a-z_]+', '', str(e))
+            error_messages.append((idx, e))
+            validated_rows.append(row.to_dict())  # Append the original row if validation fails
+    
+    # Print errors if requested
+    if print_errors and error_messages:
+        for idx, error in error_messages:
+            print(f"Row {idx}: {error}")
+
+    # Create a new DataFrame for validated rows
+    validated_df = pd.DataFrame(validated_rows)
+    valid_columns = [col for col in model.__annotations__.keys() if col in validated_df.columns]
+    
+    if drop_missing:
+        validated_df = validated_df.loc[:, valid_columns]
+
+    # Add error messages column
+    if return_errors:
+        error_col_name = "validation_errors"
+        counter = 0
+        while error_col_name in validated_df.columns:
+            counter += 1
+            error_col_name = f"validation_errors_{str(counter).zfill(2)}"
+        validated_df[error_col_name] = [None] * len(validated_df)
+        for idx, error in error_messages:
+            validated_df.at[idx, error_col_name] = str(error)
+
+    return validated_df
